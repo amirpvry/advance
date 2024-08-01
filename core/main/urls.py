06831f030d16ba828cf6ba0107f4da9fd2e77dfc
amirpvry <amirmahdi.alipooor@gmail.com>
2024-08-01 06:00:17 -0700

    7 page

--- core/main/urls.py
+++ core/main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import home_view,about_view
+from .views import home_view,about_view,trademark,online_service,copyright_view,design_view,ipserver_view,translation_view
 
 
 app_name = "main"
@@ -7,5 +7,11 @@ app_name = "main"
 urlpatterns = [
     path('', home_view, name='home'),
     path('about', about_view, name='home'),
+    path('trade', trademark, name='trade'),
+    path('online-service', online_service, name='online-service'),
+    path('translation', translation_view, name='translation'),
+    path('ip-server', ipserver_view, name='ip-server'),
+    path('design', design_view, name='design'),
+    path('copyright', copyright_view, name='copyright'),
     
 ]
--- core/main/views.py
+++ core/main/views.py
@@ -6,3 +6,19 @@ def home_view(request):
 def about_view(request):
     return render(request, 'main/about.html')
 
+def trademark(request):
+    return render(request, 'main/trademark.html')
+
+def online_service(request):
+    return render(request, 'main/online-service.html')
+
+def copyright_view(request):
+    return render(request, 'main/copyright.html')
+def translation_view(request):
+    return render(request, 'main/translation.html')
+def ipserver_view(request):
+    return render(request, 'main/ip-server.html')
+def design_view(request):
+    return render(request, 'main/design.html')
+
+
