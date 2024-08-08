11fb62be13857339953102328583d98d82775a49
amirpvry <amirmahdi.alipooor@gmail.com>
2024-08-08 16:51:09 -0700

    fine 2?

--- core/main/urls.py
+++ core/main/urls.py
@@ -9,31 +9,22 @@ urlpatterns = [
     # path("en/about", about_view, name="homes"),
     path("en/trade", trademark, name="trade"),
     path("fa/trade", fa_trademark, name="trade_fa"),
-
     path("en/online-service", online_service, name="online-service"),
     path("fa/online-service", fa_online_service, name="online-service_fa"),
-
     path("en/translation", translation_view, name="translation"),
     path("fa/translation", fa_translation_view, name="translation_fa"),
-
     path("en/ip-server", ipserver_view, name="ip-server"),
     path("fa/ip-server", fa_ipserver_view, name="ip-server_fa"),
-
     path("en/design", design_view, name="design"),
     path("fa/design", fa_design_view, name="design_fa"),
-
     path("en/copyright", copyright_view, name="copyright"),
     path("fa/copyright", fa_copyright_view, name="copyright_fa"),
-
     path("en/", homepage_view, name="homepage"),
     path("fa/", fa_homepage_view, name="homepage_fa"),
-
     path("en/services", services_view, name="services"),
     path("fa/services", fa_services_view, name="services_fa"),
-
     path("en/about_us", about_us_view, name="about_us"),
     path("fa/about_us", fa_about_us_view, name="about_us_fa"),
-    
     path("en/contact_us", contact_us_view, name="contact_us"),
     path("fa/contact_us", fa_contact_us_view, name="contact_us_fa"),
 ]
--- core/main/views.py
+++ core/main/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, "main/patent.html")
+
+
 def fa_home_view(request):
     return render(request, "main/fa_patent.html")
 
@@ -13,62 +15,79 @@ def fa_home_view(request):
 
 def trademark(request):
     return render(request, "main/trademark.html")
+
+
 def fa_trademark(request):
     return render(request, "main/fa_trademark.html")
 
 
 def online_service(request):
     return render(request, "main/online-service.html")
+
+
 def fa_online_service(request):
     return render(request, "main/fa_online-service.html")
 
 
 def copyright_view(request):
     return render(request, "main/copyright.html")
+
+
 def fa_copyright_view(request):
     return render(request, "main/copyright_fa.html")
 
 
 def translation_view(request):
     return render(request, "main/translation.html")
+
+
 def fa_translation_view(request):
     return render(request, "main/fa_translation.html")
 
+
 def ipserver_view(request):
     return render(request, "main/ip-server.html")
+
+
 def fa_ipserver_view(request):
     return render(request, "main/fa_ip-server.html")
 
 
 def design_view(request):
     return render(request, "main/design.html")
+
+
 def fa_design_view(request):
     return render(request, "main/fa_design.html")
 
 
-
-
-
 def homepage_view(request):
     return render(request, "main/home.html")
+
+
 def fa_homepage_view(request):
     return render(request, "main/fa_home.html")
 
 
 def services_view(request):
     return render(request, "main/services.html")
+
+
 def fa_services_view(request):
     return render(request, "main/fa_services.html")
 
 
-
 def about_us_view(request):
     return render(request, "main/about_us.html")
+
+
 def fa_about_us_view(request):
     return render(request, "main/fa_about_us.html")
 
 
 def contact_us_view(request):
     return render(request, "main/contact_us.html")
+
+
 def fa_contact_us_view(request):
     return render(request, "main/fa_contact_us.html")
