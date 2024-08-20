c47073d41f8d7d34e9fa55a8d146ec6574e3da64
amirpvry <amirmahdi.alipooor@gmail.com>
2024-08-20 16:47:04 -0700

    complite almost

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -1,11 +1,11 @@
-from rest_framework.routers import DefaultRouter
-from .views import PostViewSetModel, CategorySetModel
+# from rest_framework.routers import DefaultRouter
+# from .views import PostViewSetModel, CategorySetModel
 
-app_name = "api-v1"
+# app_name = "api-v1"
 
 
-router = DefaultRouter()
-router.register("post", PostViewSetModel, basename="post")
-router.register("category", CategorySetModel, basename="category")
+# router = DefaultRouter()
+# router.register("post", PostViewSetModel, basename="post")
+# router.register("category", CategorySetModel, basename="category")
 
-urlpatterns = router.urls
+# urlpatterns = router.urls
--- core/core/urls.py
+++ core/core/urls.py
@@ -48,7 +48,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
     path(
-        "blog/",
+        "",
         include("blog.urls"),
     ),
     path("accounts/", include("accounts.urls")),
